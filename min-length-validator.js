5027b12f2f27115f77112ffe75e48e800d60afc8
Pascal <freak9191@web.de>
2019-01-27 21:06:47 +0100

    Update to Polymer 3

---
+++ min-length-validator.js
@@ -0,0 +1,18 @@
+import {PolymerElement} from "@polymer/polymer";
+import {mixinBehaviors} from "@polymer/polymer/lib/legacy/class.js";
+import {IronValidatorBehavior} from "@polymer/iron-validator-behavior/iron-validator-behavior.js";
+
+class MatchPasswordsValidator extends mixinBehaviors([IronValidatorBehavior], PolymerElement){
+	// This validator only validates strings, and is only valid if
+	// the value is "cat".
+	static get properties() {
+		return {
+			minLength: Number
+		};
+	}
+  
+	validate(value) {
+		return !value || value.length >= this.minLength;
+	}
+}
+customElements.define("match-passwords-validator", MatchPasswordsValidator);
---
+++ paper-password-input.js
@@ -0,0 +1,93 @@
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import "@polymer/iron-icons/iron-icons.js";
+import "@polymer/paper-input/paper-input.js";
+import "@polymer/paper-icon-button";
+
+
+class PaperPasswordInput extends PolymerElement {
+	static get template() { return html`
+    
+        <style>
+            .visibility-icon {
+                color: var(--disabled-text-color);
+                /** Bottom align visibility button with input box */
+                height: 32px;
+                width: 32px;
+                padding: 0 4px;
+                margin-top: -6px;
+            }
+		</style>
+
+		<paper-input
+			id="input"
+			type="[[_getType(visible)]]"
+			value="{{value}}"
+			label="[[label]]"
+			name="[[name]]"
+			maxlength="[[maxlength]]"
+			disabled="[[disabled]]"
+			readonly="[[readonly]]"
+			required="[[required]]"
+			autofocus="[[autofocus]]"
+			auto-validate="[[autoValidate]]"
+			allowed-pattern="[[allowedPattern]]"
+			invalid="{{invalid}}"
+			validator="[[validator]]"
+			error-message="[[errorMessage]]"
+			char-counter="[[charCounter]]">
+			<paper-icon-button
+				slot="suffix"
+				icon="[[_getIcon(visible)]]"
+				on-tap="_toggleVisible"
+				class="visibility-icon"
+				tabindex="-1"
+				alt="[[alt]]"
+			></paper-icon-button>
+		</paper-input>`;
+	}
+
+	static get properties() {
+		return {
+			visible: {
+				type: Boolean,
+				value: false
+			},
+			value: {
+				type: String,
+				notify: true
+			},
+			invalid: {
+				type: Boolean,
+				notify: true
+			},
+			label: String,
+			name: String,
+			disabled: Boolean,
+			required: Boolean,
+			readonly: Boolean,
+			autofocus: Boolean,
+			autoValidate: Boolean,
+			validator: String,
+			errorMessage: String,
+			charCounter: Boolean,
+			maxlength: Number,
+			allowedPattern: String,
+			alt: String,
+		};
+	}
+    
+	validate() {
+		return this.$.input.validate();
+	}
+	_getIcon(visible) {
+		return visible ? "icons:visibility" : "icons:visibility-off";
+	}
+	_getType(visible) {
+		return visible ? "" : "password";
+	}
+	_toggleVisible() {
+		this.visible = !this.visible;
+	}
+}
+
+window.customElements.define("paper-password-input", PaperPasswordInput);
